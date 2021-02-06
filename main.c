1231f30563bbd86c66eed3547e5fd266b3ffa083
SoichiroSugimoto <fcsoichi1998@gmail.com>
2021-02-06 16:35:52 +0900

    this is 5th commit

--- main.c
+++ main.c
@@ -5,88 +5,92 @@
 
 int main(void)
 {
-	printf("this %i number", 17);
+	int i;
+	i = printf("this %i number", 17);
+	printf("%d\n", i);
 	printf("\n");
-
-	ft_printf("this %i number", 17);
+	i = printf("this %i number", -267);
+	printf("%d\n", i);
 	printf("\n");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-int main(void)
-{
-	printf("this %i number", 17);
+	i = printf("this %i number", 0);
+	printf("%d\n", i);
 	printf("\n");
-	// printf("this %i number", -267);
-	// printf("\n");
-	// printf("this %i number", 0);
-	// printf("\n");
-	// printf("[%i]", -1);
-	// printf("\n");
-	// printf("this %d number", 17);
-	// printf("\n");
-	// printf("this %d number", -267);
-	// printf("\n");
-	// printf("this %d number", 0);
-	// printf("\n");
-	// printf("[%7i]", -14);
-	// printf("\n");
-	// printf("[%5i]", -2562);
-	// printf("\n");
-	// printf("[%4i]", -2464);
-	// printf("\n");
-	// printf("[%-7i]", 33);
-	// printf("\n");
-	// printf("[%-7i]", -14);
-	// printf("\n");
-	// printf("[%-3i]", 0);
-	// printf("\n");
-	// printf("[%-5i]", -2562);
-	// printf("\n");
+	i = printf("[%i]", -1);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("this %d number", 17);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("this %d number", -267);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("this %d number", 0);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%7i]", -14);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%5i]", -2562);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%4i]", -2464);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%-7i]", 33);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%-7i]", -14);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%-3i]", 0);
+	printf("%d\n", i);
+	printf("\n");
+	i = printf("[%-5i]", -2562);
+	printf("%d\n", i);
+	printf("\n");
 
 	printf("\n======================\n\n");
 
-	ft_printf("this %i number", 17);
+	i = ft_printf("this %i number", 17);
+	printf("%d\n", i);
 	printf("\n");
-	// ft_printf("this %i number", -267);
-	// printf("\n");
-	// ft_printf("this %i number", 0);
-	// printf("\n");
-	// ft_printf("[%i]", -1);
-	// printf("\n");
-	// ft_printf("this %d number", 17);
-	// printf("\n");
-	// ft_printf("this %d number", -267);
-	// printf("\n");
-	// ft_printf("this %d number", 0);
-	// printf("\n");
-	// ft_printf("[%7i]", -14);
-	// printf("\n");
-	// ft_printf("[%5i]", -2562);
-	// printf("\n");
-	// ft_printf("[%4i]", -2464);
-	// printf("\n");
-	// ft_printf("[%-7i]", 33);
-	// printf("\n");
-	// ft_printf("[%-7i]", -14);
-	// printf("\n");
-	// ft_printf("[%-3i]", 0);
-	// printf("\n");
-	// ft_printf("[%-5i]", -2562);
-	// printf("\n");
+	i = ft_printf("this %i number", -267);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("this %i number", 0);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%i]", -1);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("this %d number", 17);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("this %d number", -267);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("this %d number", 0);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%7i]", -14);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%5i]", -2562);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%4i]", -2464);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%-7i]", 33);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%-7i]", -14);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%-3i]", 0);
+	printf("%d\n", i);
+	printf("\n");
+	i = ft_printf("[%-5i]", -2562);
+	printf("%d\n", i);
+	printf("\n");
 }
-
-*/
--- srcs/printf.c
+++ srcs/printf.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 05:19:05 by sosugimo          #+#    #+#             */
-/*   Updated: 2021/02/02 16:37:27 by sosugimo         ###   ########.fr       */
+/*   Updated: 2021/02/06 04:08:24 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,62 @@ int		skip_str(const char *st, f_list f)
 	return (i + 1);
 }
 
+int				compare_num(int i, int j, int order)
+{
+	int res;
+
+	res = i;
+	if (order == 1) ///大きい方
+	{
+		if (j > i)
+			res = j;
+	}
+	else
+	{
+		if (j < i)
+			res = j;
+	}
+	return (res);
+}
+
+int 			get_final_output(f_list f)
+{
+	int cnt;
+
+	cnt = 1;
+	if (f.conversion == 'c')
+		cnt = compare_num(cnt, f.field, 1);
+	else if (f.conversion == 's')
+	{
+		cnt = ft_strlen(f.str_value);
+		cnt = compare_num(cnt, f.precise, 0);
+		cnt = compare_num(cnt, f.field, 1);
+	}
+	else if (f.conversion == 'p')
+		cnt = compare_num(14, f.field, 1);
+	else if (f.conversion == 'd' || f.conversion == 'i' || f.conversion == 'u')
+	{
+		cnt = ft_strlen(ft_itoa(f.int_value));
+		cnt = (cnt, f.precise, 1);
+		cnt = (cnt, f.field, 1);
+	}
+	else
+	{
+		cnt = ft_strlen(into_hex(f.int_value, 1));
+		cnt = (cnt, f.precise, 1);
+		cnt = (cnt, f.field, 1);
+	}
+	return (cnt);
+}
+
 int				ft_printf(const char *st, ...)
 {
 	f_list	f;
 	va_list	ap;
+	int i;
 
 	va_start(ap, st);
+	i = 0;
 	while (*st)
 	{
 		while (*st && *st == '%')
@@ -55,10 +105,16 @@ int				ft_printf(const char *st, ...)
 				f.int_value = va_arg(ap, int);
 			central_arrange(f);
 			st += skip_str(st, f);
+			i += get_final_output(f);
 		}
 		write(1, st, 1);
-		st += (*st ? 1 : 0);
+		if (*st)
+		{
+			st++;
+			i++;
+		}
+		//st += (*st ? 1 : 0);
 	}
 	va_end(ap);
-	return (ft_strlen(st));
+	return (i);
 }
